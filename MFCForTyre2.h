383f666fc8bc207142b8d0f12af79aa902e68215
heyuntao@hotmail.com <heyuntao@hotmail.com>
2018-11-14 16:18:51 +0800

    Initialize MFC project for PCL.

---
+++ MFCForTyre2.h
@@ -0,0 +1,32 @@
+
+// MFCForTyre2.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFCForTyre2App: 
+// 有关此类的实现，请参阅 MFCForTyre2.cpp
+//
+
+class CMFCForTyre2App : public CWinApp
+{
+public:
+	CMFCForTyre2App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCForTyre2App theApp;
---
+++ MFCForTyre2Dlg.h
@@ -0,0 +1,56 @@
+
+// MFCForTyre2Dlg.h : 头文件
+//
+
+#pragma once
+
+#undef min
+#undef max 
+
+#include <string.h>
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/features/integral_image_normal.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/point_types.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/normal_3d_omp.h>
+#include <sstream>
+
+using namespace pcl;
+using namespace std;
+
+
+// CMFCForTyre2Dlg 对话框
+class CMFCForTyre2Dlg : public CDialogEx
+{
+// 构造
+public:
+	CMFCForTyre2Dlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCFORTYRE2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedOpenfilebutton();
+	afx_msg void OnStnClickedStaticfilepath();
+	afx_msg void OnBnClickedLoaddatabtn();
+};
